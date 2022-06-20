547d3d4ab2749a2274ea6f000adb21004cd46078
nhat8998 <86486048+nhat8998@users.noreply.github.com>
2022-06-20 12:03:01 +0700

    upload

---
+++ BaiTapLon/src/GUI/Running.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javax.swing.SwingUtilities;
+
+
+public class Running {	
+//	public static void main(String[] args) {
+//		SwingUtilities.invokeLater(new Runnable() {
+//			@Override
+//			public void run() {
+//				FormDangNhap login=new FormDangNhap();
+//				login.setVisible(true);
+//			}
+//		});
+//	}
+	public static void main(String[] args) {
+		FormDangNhap login=new FormDangNhap();
+		login.setVisible(true);
+	}
+}
